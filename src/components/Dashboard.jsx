9cfda33d7c367e95c19b98238ed21b692e5eae48
seokwon <tjrtkd95@gmail.com>
2024-08-26 22:18:26 +0900

    add: 초기화버튼 추가

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,28 +1,39 @@
 import styled from "styled-components";
 import PokemonCard from "./PokemonCard";
 import { useContext } from "react";
-import { DexContext } from "../App";
+import { AppContext } from "../App";
+import Swal from "sweetalert2";
 
 const DashboardContainer = styled.div`
   background-color: #bbb;
-  display: flex;
-  flex-direction: column;
   border-radius: 10px;
   padding: 10px;
   margin-bottom: 10px;
+  justify-content: center;
 `;
 
 const ListContainer = styled.div`
   background-color: #bbb;
-  display: grid;
-  grid-template-columns: repeat(auto-fill, minmax(150px, 1fr));
-  justify-items: center;
+  display: flex;
+  flex-flow: row wrap;
+  justify-content: center;
   background-color: #ccc;
   gap: 20px;
   border-radius: 10px;
   padding: 20px;
+  margin-bottom: 10px;
 `;
 
+const InitBtn = styled.button`
+  background-color: #ff2727;
+  width: 200px;
+  height: 30px;
+  color: white;
+  border: none;
+  border-radius: 5px;
+`;
+
+//빈배열 이미지
 const DefaultImg = () => {
   return (
     <div
@@ -42,8 +53,40 @@ const DefaultImg = () => {
     </div>
   );
 };
+
 const Dashboard = () => {
-  const { selectedPokemon } = useContext(DexContext);
+  const { selectedPokemon, setSelectedPokemon } = useContext(AppContext);
+
+  //모두 보내기 버튼
+  const initPokemon = () => {
+    if (selectedPokemon.length === 0) {
+      return Swal.fire({
+        icon: "error",
+        title: "소유한 포켓몬이 없습니다.",
+        text: "포켓몬 포획을 먼저 하세요!",
+      });
+    }
+    Swal.fire({
+      title: "포켓몬을 모두 보내시겠습니까??",
+      text: "모든 포켓몬이 야생으로 돌아갑니다.",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "예, 보내겠습니다.",
+      cancelButtonText: "취소",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire({
+          title: "포켓몬을 보냈습니다!",
+          text: " 포켓몬이 야생으로 돌아갔습니다...",
+          icon: "success",
+          confirmButtonText: "확인",
+        });
+        return setSelectedPokemon([]);
+      }
+    });
+  };
 
   const displayedPokemon = Array(6).fill(null);
   return (
@@ -73,6 +116,7 @@ const Dashboard = () => {
             })}
         </ListContainer>
       }
+      <InitBtn onClick={initPokemon}>모두 보내기</InitBtn>
     </DashboardContainer>
   );
 };
